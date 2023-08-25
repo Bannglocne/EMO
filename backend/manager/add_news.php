c4b3d4ce529a02c4298e71017cbabcb080dbd576
Lộc Nguyễn <banngloc@gmail.com>
2023-08-25 21:11:17 +0700

    ko biết là chỉnh gì

--- backend/manager/add_news.php
+++ backend/manager/add_news.php
@@ -30,7 +30,6 @@ if ($conn->connect_error) {
     <title>Trang chủ</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <script>
         function changeWeb(){
             window.location.href ='add_news.php';
